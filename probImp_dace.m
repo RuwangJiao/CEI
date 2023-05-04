4d1bb34c274d44e9183131345c172910fe6c1a02
RuwangJiao <544945252@qq.com>
2023-05-04 13:41:16 +1200

    Delete probImp_dace.m

--- probImp_dace.m
+++
@@ -1,6 +0,0 @@
-function probImp1 = probImp_dace(x, improv_y, dmodel)
-    [offspring_surrogate, dy, mse] = predictor(x, dmodel);
-    y_hat                          = offspring_surrogate;
-    SSqr                           = mse;
-    probImp1                       = normcdf((improv_y - y_hat)/sqrt(abs(SSqr)));
-end
